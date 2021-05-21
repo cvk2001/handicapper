2bf40f7e8c19b02d33b4865bfea778614b07081d
Charles Kaminski <cvk1997@gmail.com>
2021-05-21 13:48:08 -0400

    added modules and updated database

---
+++ Handicapper/Models/Round.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Handicapper.Models
+{
+    public class Round
+    {
+        public int RoundId { get; set; }
+        public int strokes { get; set; }
+        public string TeePlayed { get; set; }
+        public string Conditions { get; set; }
+        public string? Notes { get; set; }
+        public int User_Id { get; set; }
+        public int Course_Id { get; set; }
+
+    }
+}
---
+++ Handicapper/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Handicapper.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public int? handicap { get; set; }
+
+    }
+}
